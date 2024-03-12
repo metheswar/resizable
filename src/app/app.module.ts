0b41287187ab9a797bcb9c67088db8791463c3fe
methi123 <Metheswar@gmail.com>
2024-03-12 10:00:03 +0530

    deleted select lists

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { SelectItemComponent } from './select-item/select-item.component';
-import { SelectListsComponent } from './select-lists/select-lists.component';
+
 
 @NgModule({
-  declarations: [AppComponent, SelectItemComponent, SelectListsComponent],
+  declarations: [AppComponent, SelectItemComponent],
   imports: [BrowserModule, NgSelectModule],
   providers: [],
   bootstrap: [AppComponent],
